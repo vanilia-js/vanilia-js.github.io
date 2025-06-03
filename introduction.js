45004543d6d71f1f48e35f3437633bb64657dedd
vanilia-js <programistanazlecenie@proton.me>
2025-06-03 17:43:18 +0200

    ᛞᛖᛈᛚᛟᛃᛗᛖᚾᛏ

--- introduction.js
+++ introduction.js
@@ -43,6 +43,7 @@ $(c => {
         justify-content: center;
         align-items: center;
         padding: var(--font-size-1) 0;
+        margin-top: calc(var(--font-size-1) * 2);
       }
         introduction-guide blockquote div {
           text-align: right;
@@ -66,38 +67,6 @@ $(c => {
       </h2>
     </header>
     <companies-js></companies-js>
-    <section>
-      <div>
-        <h3>"modern" front-end&nbsp;development:</h3>
-        <ul>
-          <li>― trust everyone and prefer to use ready-made solutions</li>
-          <li>― use a huge framework you have a vague or no idea how it exactly works under the hood</li>
-          <li>― install packages that have an entire tree of unmanageable dependencies</li>
-          <li>― add unnecessary layers of abstraction, thinking this will help get the job done faster</li>
-          <li>― refactor your code every time a new version of the chosen framework is released</li>
-          <li>― install dependencies locally, bundle them all, split artificially, and then deploy</li>
-          <li>― transpile your code, so in the end, it doesn’t look like something you ever wrote</li>
-          <li>― have virtually no control over what exactly you are sending to the client</li>
-          <li>― depend mainly on tooling, the goodwill of someone you may not know or like</li>
-        </ul>
-      </div>
-
-      <div>  
-        <h3>not&nbsp;"modern" front-end&nbsp;development:</h3>
-        <ul>
-          <li>― trust no one and prefer to write code on your own</li>
-          <li>― write from scratch a small library you understand how it works under the hood</li>
-          <li>― add third-party code only if it is necessary, know all your dependencies</li>
-          <li>― be aware that the more abstraction you introduce, the more technical debt you create</li>
-          <li>― write code that you almost never need to refactor, do not repeat yourself</li>
-          <li>― import external resources directly via HTTP, this is how the internet works</li>
-          <li>― write code that is good enough to be interpreted by the browser as is</li>
-          <li>― have at least some control over what exactly you are sending to the client</li>
-          <li>― depend mainly on your own skills and knowledge or supervise AI strictly</li>
-        </ul>
-      </div>
-    </section>
-
     <blockquote>
       <div>
       <a  target="_blank" href="https://youtu.be/AdNJ3fydeao?t=1486"><h4>"There's only&nbsp;one reliable&nbsp;way to speed&nbsp;up your&nbsp;code and&nbsp;that&nbsp;is to get&nbsp;rid&nbsp;of&nbsp;it"</h4></a>
